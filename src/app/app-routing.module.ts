2fdf7a0d0c952e2470cf8501620d8a05978520e0
Andrii Laptiiev <andy.megadev@gmail.com>
2023-06-28 22:15:05 +0300

    Implement login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-const routes: Routes = [];
+  { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
 ];
 
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  onSubmit() {
+    // TODO: send post request to server
+  }
+}
